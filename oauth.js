13301cf89a262640a491eec2ea13dce65e2be6ce
YuzhongHuang <yuzhong.huang@students.olin.edu>
2015-04-21 23:09:49 -0400

    delete stuff

--- oauth.js
+++
@@ -1,9 +0,0 @@
-var ids = {
-  facebook: {
-    clientID: '817662698315463',
-    clientSecret: '61bea842628a3141f1209daeab94f4f6',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
-  }
-};
- 
-module.exports = ids;
